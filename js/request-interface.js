ed77b1d26efb68511ef5a73a18c8b0364b9f4206
victor tarus <victorkibet25@gmail.com>
2017-05-31 11:18:17 +0300

    fix error

--- js/request-interface.js
+++ js/request-interface.js
@@ -1,31 +1,13 @@
-var apiKey =require('../.env').apiKey;
+var Request=require('../js/request.js').requestModule;
+
 //get user input from user
 
 
 $(document).ready(function(){
   $("form#search").submit(function(){
-    event.preventDefault()
-    var UserName =$("#username").val();
+    event.preventDefault();
+    var user =$("#username").val();
     $('#username').val('');
-    $.get('https://api.github.com/users/'+UserName+'?access_token=' + apiKey).then(function (response) {
-         console.log(response);
-         $('.searched').text('You have searched for '+ UserName+'.');
 
-    }).fail(function (error) {
-        $("#found").append(error.responseJSON.message);
-    });
-       var repos=$(".repo").val();
-       $('.repo').val();
-       $.get('https://api.github.com/users/'+UserName+ '?access_token='+apiKey).then(function(response){
-
-         for(var i=0;i<response.length+1;i++){
-           if (response[i].description===null) {
-             displayrepos(response[i].name,response[i].description.response.);
-           }
-
-         }.fail(function(error){
-           $(".repo").append(error.responseJSON.message);
-         })
-       })
   });
   });
--- js/request.js
+++ js/request.js
@@ -1,13 +1,17 @@
-// var user=require('./js/request-interface');
-//
-//  Request=function(){
-//
-// }
-// Request.prototype.getRequest = function(UserName,displayFunction){
-//   $.get('https://api.github.com/users/'+ UserName+'repos?access_token=' + apiKey+).then(function(response){
-//     displayFunction(UserName);
-//   }).fail(function(error){
-//     $('.searched').text.(error.responseJSON.message);
-//   });
-// };
-//  exports.requestModule=Request;
+var user=require('../js/request-interface.js');
+var apiKey =require('../.env').apiKey;
+
+ function Request(){ //request object
+
+}
+
+  $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function (response) {
+       console.log(response);
+       $(".Found").text("The User's name is:"+user);
+
+  }).fail(function (error) {
+
+      console.log("failed");
+  });
+
+ exports.requestModule=Request;
