6caa1c18e35311a7192e48340fa89020b03ed2e0
victor tarus <victorkibet25@gmail.com>
2017-06-08 09:43:01 +0300

    created a module and updated the html

--- js/request-interface.js
+++ js/request-interface.js
@@ -1,8 +1,25 @@
+var UserSearch =require('./js/request.js').requestmodule;
+
+var displayResults=function(username,publicRepo){
+  $('#found').show();
+  $("#repo").text(foundRepo);
+}
+
+function displayResults(description){
+  if(description ===null){
+return"none found";
+  }else
+  {
+      return description;
+  }
+}
 
 $(document).ready(function(){
-  $('#search').click(function(){
-
-    var Username =$('#username').val();
+  $('#search').submit(function(event){
+    currentUserSearch=new UserSearch();
+    var username =$('#username').val();
     $('username').val();
+    currentUserSearch.userLookup(username,displayResults)
+    currentUserSearch.repoLookup(username,foundRepo);
   })
 });
--- js/request.js
+++ js/request.js
@@ -1,19 +1,16 @@
 var apiKey=require('./../env');
-function User(){
+function UserSearch(){
 
 }
 User.prototype.userLookup = function (username,displayResults) {
   $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
     displayResults(username,response.html_url,response.public_repos);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
   });
-
 };
-User.prototype.userLookup=function(username,findRepo){
+User.prototype.repoLookup=function(username,foundRepo){
   $get('https://api.github.com/users/'+username+'/repos?access_token='+apiKey).then(function(response){
     console.log(response);
-    findRepo(response);
+    foundRepo(response);
   });
 };
-exports.requestmodule=User;
+exports.requestmodule=UserSearch;
